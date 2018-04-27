9b2e798fc7f8342c0ea6ce31cba478521af26f66
jasau <38782739+jasau@users.noreply.github.com>
2018-04-27 16:13:00 +0200

    compare date + show text

--- MMM-DayMessage.js
+++ MMM-DayMessage.js
@@ -3,15 +3,38 @@
 Module.register("MMM-DayMessage",{
     // Default module config.
     defaults: {
-        text: "Hello World! From WebStorm!"
+        day: "27/04/2018",
+        text: "Happy Birthday!",
+        updateInterval: 30000
     },
 
+    // Define required scripts.
+    getScripts: function() {
+        return ["moment.js"];
+    },
 
+    // Define start sequence.
+    start: function() {
+        Log.info("Starting module: " + this.name);
+
+        var self = this;
+    },
+
+    compare: function() {
+        var today = moment().format("DD/MM/YYYY");
+        var display = "";
+
+        if (today == this.config.day) {
+            display = this.config.text;
+        }
+
+        return display;
+    },
 
     // Override dom generator.
     getDom: function() {
         var wrapper = document.createElement("div");
-        wrapper.innerHTML = this.config.text;
+        wrapper.innerHTML = this.compare();
         return wrapper;
     }
 });
